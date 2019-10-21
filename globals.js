2c0de194fdc4393482179e52f8fc5201ef1ca199
Christopher Albright <calbright1998@gmail.com>
2019-10-21 11:38:09 -0700

    added some more style to DiscoverFeed and added global.js to store endpoints as global variables

---
+++ globals.js
@@ -0,0 +1,9 @@
+const URLs = {
+    HOSTNAME: 'http://my-tunes-be.herokuapp.com',
+    ENDPOINTS = {
+        "USER_PROFILE_MODEL": "/userProfileModel",
+        "TOP_TRACKS": "/topTracks",
+        "TOP_ARTISTS": "/topArtists",
+        "USER_PROFILE": "/userProfile"
+    }
+};
