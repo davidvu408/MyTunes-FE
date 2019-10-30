61dde147238bfd52c00bd32e967609ee20eca4fa
Christopher Albright <calbright1998@gmail.com>
2019-10-30 10:45:59 -0700

    Created branch for data visualization on the user profile

--- globals.js
+++
@@ -1,9 +0,0 @@
-const URLs = {
-    HOSTNAME: 'http://my-tunes-be.herokuapp.com',
-    ENDPOINTS = {
-        "USER_PROFILE_MODEL": "/userProfileModel",
-        "TOP_TRACKS": "/topTracks",
-        "TOP_ARTISTS": "/topArtists",
-        "USER_PROFILE": "/userProfile"
-    }
-};
