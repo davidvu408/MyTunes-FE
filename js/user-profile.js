ecfc9631015af30fe66674e3c676fc08573f011e
Justin Singh <justinsingh2015@gmail.com>
2019-12-04 04:15:10 -0800

    Integrate popularity preference graph

--- js/user-profile.js
+++ js/user-profile.js
@@ -1,39 +1,122 @@
 function renderUserProfile(userProfileObj) {
-    if (!userProfileObj) {
-        userProfileObj = testUserProfileObj;
-    }
-    // Render user image
-    const userImageRow = document.createElement('div');
-    userImageRow.className = "row";
-    const userImageCol = document.createElement('div');
-    userImageCol.className = "col-lg";
-    userImageCol.align = "center";
-    const userImage = document.createElement('img');
-    if (userProfileObj.images[0])
-        userImage.src = userProfileObj.images[0]['url'];
-    else
-        userImage.src = 'https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/linus-van-pelt-charlie-browns-all-stars-46.9.jpg'
-    userImage.className = "rounded-circle mt-4";
-    userImage.style = "width:12em;height:12em";
-    userImageCol.appendChild(userImage);
-    userImageRow.appendChild(userImageCol);
-    document.getElementById('user-info-container').appendChild(userImageRow);
-    // Render user display name
-    const userDisplayNameRow = document.createElement('div');
-    userDisplayNameRow.className = "row";
-    const userDisplayNameCol = document.createElement('div');
-    userDisplayNameCol.className = "col-lg";
-    userDisplayNameCol.align = "center";
-    const userDisplayNameHeader = document.createElement('h2');
-    userDisplayNameHeader.className = "text-white";
-    userDisplayNameHeader.style = "padding-top:0.5em;"
-    userDisplayNameHeader.innerHTML = 'Welcome, ' + userProfileObj.displayName + '!';
+     if (!userProfileObj) {
+          userProfileObj = testUserProfileObj;
+     }
+     // Render user image
+     const userImageRow = document.createElement('div');
+     userImageRow.className = "row";
+     const userImageCol = document.createElement('div');
+     userImageCol.className = "col-lg";
+     userImageCol.align = "center";
+     const userImage = document.createElement('img');
+     if (userProfileObj.images[0])
+          userImage.src = userProfileObj.images[0]['url'];
+     else
+          userImage.src = 'https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/linus-van-pelt-charlie-browns-all-stars-46.9.jpg'
+     userImage.className = "rounded-circle mt-4";
+     userImage.style = "width:12em;height:12em";
+     userImageCol.appendChild(userImage);
+     userImageRow.appendChild(userImageCol);
+     document.getElementById('user-info-container').appendChild(userImageRow);
+     // Render user display name
+     const userDisplayNameRow = document.createElement('div');
+     userDisplayNameRow.className = "row";
+     const userDisplayNameCol = document.createElement('div');
+     userDisplayNameCol.className = "col-lg";
+     userDisplayNameCol.align = "center";
+     const userDisplayNameHeader = document.createElement('h2');
+     userDisplayNameHeader.className = "text-white";
+     userDisplayNameHeader.style = "padding-top:0.5em;"
+     userDisplayNameHeader.innerHTML = 'Welcome, ' + userProfileObj.displayName + '!';
 
-    userDisplayNameCol.appendChild(userDisplayNameHeader);
+     userDisplayNameCol.appendChild(userDisplayNameHeader);
 
-    userDisplayNameRow.appendChild(userDisplayNameCol);
-    document.getElementById('user-info-container').appendChild(userDisplayNameRow);
+     userDisplayNameRow.appendChild(userDisplayNameCol);
+     document.getElementById('user-info-container').appendChild(userDisplayNameRow);
 }
+
+
+function renderPopularityPreference(userProfileObj) {
+     function formatData(obj){
+          var rtnObj = [];
+          let keys = Object.keys(obj);
+
+          for(var i in obj){
+               var temp;
+               if (keys[i] % 5 == 0)
+                    rtnObj.push({"popularity": keys[i], "value": obj[i]});
+               console.log(keys[i] + ": " + obj[i]);
+          }
+	      return rtnObj;
+     }
+
+     if (!userProfileObj) {
+          userProfileObj = testUserProfileObj;
+     }
+
+     const data = formatData(userProfileObj['songPopularity']); 
+
+     // set the dimensions and margins of the graph
+     var margin = {top: 20, right: 30, bottom: 40, left: 90},
+         width = 800 - margin.left - margin.right,
+         height = 800 - margin.top - margin.bottom;
+     var svg3 = d3.select("#song-pop-graph")
+              .append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+              .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
+     // Add X axis
+     var x = d3.scaleLinear()
+              .range([0, width])
+              .domain([0, 100]);
+     svg3.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x))
+          .selectAll("text")
+          .attr("transform", "translate(-10,0)rotate(-45)")
+          .style("text-anchor", "end")
+          .style("stroke", "white")
+          .attr("class", "xAxis");
+     // Y axis
+     var y = d3.scaleBand()
+              .range([0, height])
+              .domain(data.map(function(d) { return d.popularity }))
+              .padding(.1);
+     svg3.append("g")
+          .call(d3.axisLeft(y))
+          .style("stroke", "white")
+          .attr("class", "yAxis");
+     //Bars
+     svg3.selectAll("myRect")
+          .data(data)
+          .enter()
+          .append("rect")
+          .attr("x", x(0))
+          .attr("y", function(d) { return y(d.popularity); })
+          .attr("width", function(d) { return x(d.value); })
+          .attr("height", y.bandwidth())
+          .attr("fill", "#69b3a2");
+     // text label for the x axis
+     svg3.append("text")             
+          .attr("transform",
+                "translate(" + (width/2) + " ," + 
+                (height + margin.top + 20) + ")")
+          .style("text-anchor", "middle")
+          .style("stroke", "white")
+          .text("Recent Play Count");
+     // text label for the y axis
+     svg3.append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 0 - margin.left)
+          .attr("x",0 - (height / 2))
+          .attr("dy", "1em")
+          .style("text-anchor", "middle")
+          .style("stroke", "white")
+          .text("Streaming Popularity");
+    
+     }
 
 function renderTimeEraPreference(userProfileObj) {
      if (!userProfileObj) {
@@ -55,7 +138,7 @@ function renderTimeEraPreference(userProfileObj) {
      // load the data
      // Add X axis
      var x = d3.scaleLinear()
-              .domain([0, 150])
+              .domain([0, 100])
               .range([0, width]);
      svg2.append("g")
           .attr("transform", "translate(0," + height + ")")
@@ -226,6 +309,7 @@ fetch(userProfileUrl)
           renderFavoriteAlbums(jsondata);
           renderFavoriteGenres(jsondata);
           renderTimeEraPreference(jsondata);
+          renderPopularityPreference(jsondata);
      });
 
 
